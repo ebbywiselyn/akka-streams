4a726403b25bdf1ed49aaca4421e52f9749e46ae
Ebby Wiselyn <ebbywiselyn@gmail.com>
2016-02-09 00:08:40 +0530

    Add a simple broadcast, which can take a source of range 1 to 10 and create two flows one which multiplies by 2, and another which multiplies by 3, write it to two different sinks.

--- src/main/scala/streams/SimpleBroadcast.scala
+++ src/main/scala/streams/SimpleBroadcast.scala
@@ -1,4 +1,29 @@
+import akka.actor.ActorSystem
+import akka.stream.{ClosedShape, ActorMaterializer}
+import akka.stream.scaladsl._
+
+import scala.concurrent.Future
+
 object SimpleBroadcast {
+  implicit val system = ActorSystem("simple-log")
+  implicit val materializer = ActorMaterializer()
+  val source = Source (1 to 10)
+
   def main(arr: Array[String]): Unit = {
+     val g = createGraph
+     g.run()
+    println ("done")
+  }
+
+  def createGraph = {
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val bcast = b.add(Broadcast[Int](2))
+      source ~> bcast.in
+      bcast.out(0) ~> Flow[Int].map(e => e * 2) ~> Sink.foreach[Int](println(_))
+      bcast.out(1) ~> Flow[Int].map(e => e * 3) ~> Sink.foreach[Int](println(_))
+      ClosedShape
+    })
   }
 }
